489467b59b3b51d8d0f33c3aaa04b06211c22672
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-25 21:08:38 +0100

    hello

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router =
+        axum::Router::<()>::new().route("/ready", axum::routing::get(async || "hello, world!"));
+
+    let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+
+    axum::serve(listener, router).await.unwrap();
 }
