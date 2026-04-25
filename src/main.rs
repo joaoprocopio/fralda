f8eed17873bb3c4f087eb802ec9c4a1f6a98c15e
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-25 21:21:12 +0100

    vai

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,63 @@
 use anyhow::Result;
 use axum;
-use fralda::ext;
 use std::process;
-use tokio::net::TcpListener;
+use tokio::{
+    net::TcpListener,
+    runtime,
+    signal::unix::{SignalKind, signal},
+};
+
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+pub fn init_tracing() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_target(true)
+                .with_line_number(true)
+                .with_file(true),
+        )
+        .init();
+}
+
+pub fn new_runtime() -> runtime::Runtime {
+    match runtime::Builder::new_current_thread().enable_all().build() {
+        Ok(rt) => rt,
+        Err(err) => {
+            tracing::error!("tokio runtime boot failed: {err}");
+            std::process::exit(1);
+        }
+    }
+}
+
+pub async fn shutdown_signal() -> Result<impl Future<Output = ()>> {
+    let mut terminate = signal(SignalKind::terminate())?;
+    let mut interrupt = signal(SignalKind::interrupt())?;
+
+    let signal = async move {
+        tokio::select! {
+            Some(_) = terminate.recv() => {
+                tracing::info!("SIGTERM received");
+            },
+            Some(_) = interrupt.recv() => {
+                tracing::info!("SIGINT received");
+            }
+        }
+    };
+
+    Ok(signal)
+}
 
 fn main() {
-    ext::tracing::init_tracing();
-    let runtime = ext::tokio::new_runtime();
+    init_tracing();
+    let runtime = new_runtime();
 
-    // runtime.handle().clone()
-    runtime.block_on(run()).unwrap_or_else(|err| {
-        tracing::error!("server boot failed with: {}", err);
-        process::exit(1)
-    })
+    runtime
+        .block_on(run(/* runtime.handle().clone() */))
+        .unwrap_or_else(|err| {
+            tracing::error!("server boot failed with: {}", err);
+            process::exit(1)
+        })
 }
 
 async fn run() -> Result<()> {
