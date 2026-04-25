1c1b0ee4ac1ac9f4e52c3c0e93dd4a8125c9f130
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-25 21:18:07 +0100

    vai

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
+use anyhow::Result;
 use axum;
+use fralda::ext;
+use std::process;
 use tokio::net::TcpListener;
 
-#[tokio::main]
-async fn main() {
+fn main() {
+    ext::tracing::init_tracing();
+    let runtime = ext::tokio::new_runtime();
+
+    // runtime.handle().clone()
+    runtime.block_on(run()).unwrap_or_else(|err| {
+        tracing::error!("server boot failed with: {}", err);
+        process::exit(1)
+    })
+}
+
+async fn run() -> Result<()> {
     let router =
         axum::Router::<()>::new().route("/ready", axum::routing::get(async || "hello, world!"));
 
-    let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8000").await?;
 
-    axum::serve(listener, router).await.unwrap();
+    axum::serve(listener, router).await?;
+
+    Ok(())
 }
